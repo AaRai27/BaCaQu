6787a7c144257236cb250b6d9802b084d8662ddd
AaRai27 <raihangeorgia@gmail.com>
2021-01-29 14:41:52 +0700

    Commit Sebelum Backend Login
    Coomit Sebelum Backend Login

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -3,10 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Account extends CI_Controller
 {
-    function __construct() {
-        parent::__construct();
-        $this->load->model('Account');
-    }
 
     function __construct()
     {
@@ -20,19 +16,6 @@ class Account extends CI_Controller
         $this->load->view('account/login-regis');
         $this->load->view('templates/footer-login');
     }
-
-    public function login() {
-        $username = $this->input->post('username', true),
-        $password = $this->input->post('password', true)
-        $masuk = $this->Account->login($username, $password);
-        if ($masuk) {
-            $this->session->set_userdata('username', $username);
-            redirect(base_url());
-        } else {
-            redirect('account/index');
-        }
-    }
-
 
     public function login()
     {
@@ -53,5 +36,3 @@ class Account extends CI_Controller
         redirect(base_url());
     }
 }
-    
-
