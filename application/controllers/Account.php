00ba1ce94ef19523ac63ec46ca616f4f7a8368b5
AaRai27 <raihangeorgia@gmail.com>
2021-01-29 14:12:01 +0700

    Perbaikan
    Perbaikan

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -6,6 +6,31 @@ class Account extends CI_Controller
 
     public function index()
     {
-        $this->load->view('view');
+        $this->load->view('templates/header-login');
+        $this->load->view('account/login-regis');
+        $this->load->view('templates/footer-login');
     }
+<<<<<<< Updated upstream
+=======
+
+    public function login() {
+        $username = $this->input->post('username', true),
+        $password = $this->input->post('password', true)
+        $masuk = $this->Account->login($username, $password);
+        if ($masuk) {
+            $this->session->set_userdata('username', $username);
+            redirect(base_url());
+        } else {
+            redirect('account/index');
+        }
+    }
+
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect(base_url());
+    }
+}
+
+    
+>>>>>>> Stashed changes
 }
