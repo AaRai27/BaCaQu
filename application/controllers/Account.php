6c95eb7d0159071a1096752bd472ceb185a874f7
AzureStream <46711225+AzureStream@users.noreply.github.com>
2021-01-29 13:38:50 +0700

    minor configuration and login/logout

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -3,9 +3,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Account extends CI_Controller
 {
+    function __construct() {
+        parent::__construct();
+        $this->load->model('Account');
+    }
 
     public function index()
     {
         $this->load->view('view');
+    }
+
+    public function login() {
+        $username = $this->input->post('username', true),
+        $password = $this->input->post('password', true)
+        $masuk = $this->Account->login($username, $password);
+        if ($masuk) {
+            $this->session->set_userdata('username', $username);
+            redirect(base_url());
+        } else {
+            redirect('account/index');
+        }
+    }
+
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect(base_url());
     }
 }
