e0bc23db8d02ade375f2d3e60c4c4a373ca7e245
AaRai27 <raihangeorgia@gmail.com>
2021-01-29 23:16:19 +0700

    Al-Quran API
    Al-Quran API

--- application/models/Santri.php
+++ application/models/Santri.php
@@ -1,26 +1,34 @@
 <?php
 
-    class ModelSantri extends CI_Model {
+class Santri extends CI_Model
+{
 
-        function daftar($data) {
-            return $this->db->insert('santri', $data);
-        }
-
-        function get_akun_id($id) {
-            $this->db->where('id_santri', $id);
-            return $this->db->get('santri');
-        }
-
-        function update_akun($id, $data) {
-            $this->db->where('id_santri', $id);
-            return $this->db->update('santri', $data);
-        }
-
-        function delete_akun($$id) {
-            $this->db->where('id_santri',$id);
-            return $this->db->delete('santri');
-        }
-
+    function daftar($data)
+    {
+        return $this->db->insert('santri', $data);
     }
 
-?>
+    function get_akun_id($id)
+    {
+        $this->db->where('id_santri', $id);
+        return $this->db->get('santri')->row_array();
+    }
+
+    function get_akun_by_ustadz($id_ustadz)
+    {
+        $this->db->where('id_ustadz', $id_ustadz);
+        return $this->db->get('santri')->result_array();
+    }
+
+    function update_akun($id, $data)
+    {
+        $this->db->where('id_santri', $id);
+        return $this->db->update('santri', $data);
+    }
+
+    function delete_akun($id)
+    {
+        $this->db->where('id_santri', $id);
+        return $this->db->delete('santri');
+    }
+}
--- application/models/Ustadz.php
+++ application/models/Ustadz.php
@@ -1,26 +1,28 @@
 <?php
 
-    class ModelUstadz extends CI_Model {
+class Ustadz extends CI_Model
+{
 
-        function daftar($data) {
-            return $this->db->insert('ustadz', $data);
-        }
-
-        function get_akun_id($id) {
-            $this->db->where('id_ustadz', $id);
-            return $this->db->get('ustadz');
-        }
-
-        function update_akun($id, $data) {
-            $this->db->where('id_ustadz', $id);
-            return $this->db->update('ustadz', $data);
-        }
-
-        function delete_akun($$id) {
-            $this->db->where('id_ustadz',$id);
-            return $this->db->delete('ustadz');
-        } 
-
+    function daftar($data)
+    {
+        return $this->db->insert('ustadz', $data);
     }
 
-?>
+    function get_akun_id($id)
+    {
+        $this->db->where('id_ustadz', $id);
+        return $this->db->get('ustadz')->row_array();
+    }
+
+    function update_akun($id, $data)
+    {
+        $this->db->where('id_ustadz', $id);
+        return $this->db->update('ustadz', $data);
+    }
+
+    function delete_akun($id)
+    {
+        $this->db->where('id_ustadz', $id);
+        return $this->db->delete('ustadz');
+    }
+}
