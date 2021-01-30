610c4e3ae9697cac7a331ab0017174d90ed78890
AaRai27 <raihangeorgia@gmail.com>
2021-01-31 00:24:06 +0700

    Bismillah Push Lagi
    Bismillah Push Lagi

--- application/controllers/Santri.php
+++ application/controllers/Santri.php
@@ -6,18 +6,43 @@ class Santri extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('ModelLaporan');
         $this->load->model('ModelSantri');
-        $this->load->model('ModelUstadz');
     }
 
     public function index() {
+<<<<<<< Updated upstream
         $data['user'] = $this->ModelSantri->get_akun_id($this->session->userdata('id'));
         $tmp = $data['user']
         $data['ustadz'] = $this->ModelUstadz->get_akun_id($tmp['id_ustadz']);
         $data['laporan'] = $this->ModelLaporan->get_laporan_santri($this->session->userdata('id'));
 
+=======
+>>>>>>> Stashed changes
         $this->load->view('templates/header-dashboard');
-        $this->load->view('dashboardSantri', $data);
+        // $this->load->view('dashboardUstad');
         $this->load->view('templates/footer');
+<<<<<<< Updated upstream
     }
+=======
+    }
+
+    public function set_santri()
+    {
+        $data_santri = array(
+            'id_santri' => $this->input->post('id', true),
+            'id_ustadz' => 'empty';
+            'nama' => $this->input->post('nama', true),
+            'level' => 1,
+            'telepon' => $this->input->post('telepon', true)
+        );
+        $cek = $this->ModelSantri->daftar($data_santri);
+
+        if ($cek) {
+            $this->session->set_flashdata('setting_profil', 'sukses');
+        } else {
+            $this->session->set_flashdata('setting_profil', 'gagal');
+        }
+        redirect('santri', 'refresh');
+    }
+}
+>>>>>>> Stashed changes
