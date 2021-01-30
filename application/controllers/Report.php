b9acde9a1cf489d72bb1e6fc6acae40b48201f5e
AaRai27 <raihangeorgia@gmail.com>
2021-01-30 20:14:22 +0700

    Iqra Fundamental
    Iqra Fundamental

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -78,6 +78,59 @@ class Report extends CI_Controller
         $this->load->view('templates/footer');
     }
 
+    public function read_iqra()
+    {
+        // $data['iqra'] = $this->ModelQuran->get_all_iqra();
+        $this->load->view('templates/header');
+        $this->load->view('ngaji/iqraMenu');
+        $this->load->view('templates/footer');
+    }
+
+    public function view_iqra($jilid)
+    {
+        $query = $this->db->query("SELECT halaman FROM iqra");
+        // $total = 
+
+        $config['base_url'] = base_url('report/view_iqra/' . $jilid); //site url
+        $config['total_rows'] = $query->num_rows();; //$this->db->count_all('produk') //total row
+        $config['per_page'] = 1;  //show record per halaman
+        $config["uri_segment"] = 4;  // uri parameter
+        // $choice = $config["total_rows"] / $config["per_page"];
+        // $config["num_links"] = floor($choice);
+
+        $config['first_link']       = 'First';
+        $config['last_link']        = 'Last';
+        $config['next_link']        = '<div class="col"><a onclick="back()" href=""><img src="' . base_url('assets/images/arrow.svg') . '" width="15px" alt=""></a></div>';
+        $config['prev_link']        = '<div class="col"><a onclick="back()" href=""><img src="' . base_url('assets/images/arrow.svg') . '" style="transform:rotate(180deg);" width="15px" alt=""></a></div>';
+        $config['full_tag_open']    = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center">';
+        $config['full_tag_close']   = '</ul></nav></div>';
+        $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+        $config['num_tag_close']    = '</span></li>';
+        $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+        $config['cur_tag_close']    = '<span class="sr-only">(current)</span></span></li>';
+        $config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['next_tagl_close']  = '<span aria-hidden="true">&raquo;</span></span></li>';
+        $config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['prev_tagl_close']  = '</span>Next</li>';
+        $config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+        $config['first_tagl_close'] = '</span></li>';
+        $config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['last_tagl_close']  = '</span></li>';
+
+        $this->pagination->initialize($config);
+        $data['page'] = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+
+        //     //panggil function get_mahasiswa_list yang ada pada mmodel mahasiswa_model. 
+        // $data['data_ayat'] = $this->ModelQuran->get_ayat($config["per_page"], $data['page']);
+
+        $data['data_iqra'] = $this->ModelQuran->get_iqra($config["per_page"], $data['page']);
+        $data['pagination'] = $this->pagination->create_links();
+
+        $this->load->view('templates/header');
+        $this->load->view('ngaji/iqra', $data);
+        $this->load->view('templates/footer');
+    }
+
     // public function buat_laporan() {
     //     $data = array(
     //         'id_ustadz' => $this->input->post('id_ustadz', true),
