2c2ff0d31744cd77be5cdd14f35635f64d00839c
AaRai27 <raihangeorgia@gmail.com>
2021-01-30 14:31:44 +0700

    Login, Signup, Cek Username
    Login, Signup, Cek Username

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -28,7 +28,7 @@ class Account extends CI_Controller
             redirect('account');
         } else {
             $this->_login();
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Wrong Username/Password</div>');
+            $this->session->set_flashdata('register', '<div class="alert alert-danger" role="alert">Wrong Username/Password</div>');
         }
     }
 
@@ -47,11 +47,11 @@ class Account extends CI_Controller
                 $this->session->set_userdata($data);
                 redirect('ustadz');
             } else {
-                $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Wrong Username/Password</div>');
+                $this->session->set_flashdata('register', '<div class="alert alert-danger" role="alert">Wrong Username/Password</div>');
                 redirect('account');
             }
         } else {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">You are not an admin</div>');
+            $this->session->set_flashdata('register', '<div class="alert alert-danger" role="alert">You are not an admin</div>');
             redirect('account');
         }
     }
@@ -91,6 +91,7 @@ class Account extends CI_Controller
         redirect('account');
     }
 
+<<<<<<< Updated upstream
     public function daftar_ustadz()
     {
         $next_id = $this->ModelAccount->count_ustadz() + 1;
@@ -116,21 +117,85 @@ class Account extends CI_Controller
             // redirect home
         } else {
             // register gagal
+=======
+    public function daftar()
+    {
+        $this->form_validation->set_rules('username', 'Username', 'required|is_unique[account.username]');
+        $this->form_validation->set_rules('role', 'Role', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('password1', 'Confirmation Password', 'required|matches[password]');
+
+
+        $role = $this->input->post('role', true);
+        if ($role == 1) {
+            $next_id = $this->ModelAccount->count_ustadz() + 1;
+            $role = 1;
+            $data = array(
+                'user_id' => 'u-' . $next_id,
+                'username' => $this->input->post('username', true),
+                'password' => $this->input->post('password', true),
+                'role' => $role
+            );
+        } else {
+            $next_id = $this->ModelAccount->count_santri() + 1;
+            $role = 2;
+            $data = array(
+                'user_id' => 's-' . $next_id,
+                'username' => $this->input->post('username', true),
+                'password' => $this->input->post('password', true),
+                'role' => $role
+            );
         }
+
+        $cek_username = $this->ModelAccount->cek_username($data['username']);
+        if (!$cek_username) {
+            $cek = $this->ModelAccount->daftar($data);
+        } else {
+            $cek = false;
+        }
+
+        if ($cek) {
+            $this->session->set_flashdata('register', '<div class="alert alert-success alert-dismissible fade show" role="alert">Akun Berhasil Dibuat<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button> </div>');
+        } else {
+            $this->session->set_flashdata('register', '<div class="alert alert-danger alert-dismissible fade show" role="alert">Akun Gagal Dibuat<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button> </div>');
+>>>>>>> Stashed changes
+        }
+        redirect('account');
     }
 
+    // public function daftar_ustadz()
+    // {
+    //     $next_id = $this->ModelAccount->count_ustadz() + 1;
+    //     $data = array(
+    //         'user_id' => 'u' . $next_id,
+    //         'username' => $this->input->post('username', true),
+    //         'password' => $this->input->post('password', true),
+    //         'role' => 1
+    //     );
 
-    public function daftar_santri()
-    {
-        $next_id = $this->ModelAccount->count_santri() + 1;
-        $data = array(
-            'user_id' => 's' . $next_id,
-            'username' => $this->input->post('username', true),
-            'password' => $this->input->post('password', true),
-            'role' => 2
-        );
-        $cek1 = $this->ModelAccount->daftar($data);
+    //     $cek1 = $this->ModelAccount->daftar($data);
 
+    //     $data_ustadz = array(
+    //         'id_ustadz' => $data['user_id'],
+    //         'nama' => $this->input->post('nama', true),
+    //         'deskripsi' => $this->input->post('link', true),
+    //         'telepon' => $this->input->post('telepon', true)
+    //     );
+    //     $cek2 = $this->ModelUstadz->daftar($data_ustadz);
+
+    //     if ($cek1 and $cek2) {
+    //         $this->session->set_flashdata('daftar', 'sukses');
+    //     } else {
+    //         $this->session->set_flashdata('daftar', 'gagal');
+    //     }
+    // }
+
+
+<<<<<<< Updated upstream
         $data_santri = array(
             'id_santri' => $data['user_id'],
             'id_ustadz' => '',
@@ -147,6 +212,35 @@ class Account extends CI_Controller
             // register gagal
         }
     }
+=======
+    // public function daftar_santri()
+    // {
+    //     $next_id = $this->ModelAccount->count_santri() + 1;
+    //     $data = array(
+    //         'user_id' => 's' . $next_id,
+    //         'username' => $this->input->post('username', true),
+    //         'password' => $this->input->post('password', true),
+    //         'role' => 2
+    //     );
+    //     $cek1 = $this->ModelAccount->daftar($data);
+
+    //     $data_santri = array(
+    //         'id_santri' => $data['user_id'],
+    //         'id_ustadz' => '',
+    //         'nama' => $this->input->post('nama', true),
+    //         'level' => $this->input->post('level', true),
+    //         'telepon' => $this->input->post('telepon', true)
+    //     );
+    //     $cek2 = $this->ModelSantri->daftar($data_santri);
+
+    //     if ($cek1 and $cek2) {
+    //         $this->session->set_flashdata('daftar', 'sukses');
+    //         // redirect home
+    //     } else {
+    //         // register gagal
+    //     }
+    // }
+>>>>>>> Stashed changes
 
 
     public function update_akun_santri($id_santri)
